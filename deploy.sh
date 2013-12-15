2f8bd036636301132fcbd317924addf3ac14cca9
Martin Vanek <anthavio@post.cz>
2013-12-15 17:35:21 +0000

    com.anthavio -> net.anthavio

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-REPO_OPTS="-Durl=http://oss.sonatype.org/content/repositories/snapshots -DrepositoryId=sonatype-oss-snapshots"
+#REPO_OPTS="-Durl=http://oss.sonatype.org/content/repositories/snapshots -DrepositoryId=sonatype-oss-snapshots"
+REPO_OPTS="-Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2 -DrepositoryId=sonatype-oss-staging"
+
 doDeploy() {
-	#mvn install -f $1
-	mvn deploy:deploy-file -Dfile=$1 -DpomFile=$1 $REPO_OPTS
+	#mvn deploy:deploy-file -Dfile=$1 -DpomFile=$1 $REPO_OPTS
+	mvn gpg:sign-and-deploy-file -Dfile=$1 -DpomFile=$1 $REPO_OPTS
 }
 
 if [ "$1" != "" ]; then
